83cbca43bf1cf0769f6ad3f5f347187b26c87ed6
Hossain <maherhossain10@gmail.com>
2021-11-16 00:09:20 +0600

    updated

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 const app = express();
-const port = process.env.PORT | 5000;
+const port = process.env.PORT || 5000;
 
 
 // Middleware
